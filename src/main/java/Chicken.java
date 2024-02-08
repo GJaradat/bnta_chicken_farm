365aa2dd830673bd4c5b6aa7fd2ac7f03be633be
Gabriel Dzharadat <dzh.gabriel@gmail.com>
2024-02-08 11:19:33 +0000

    Created Chicken class. Added getters/setters and tests.

---
+++ src/main/java/Chicken.java
@@ -0,0 +1,29 @@
+public class Chicken {
+
+    private String name;
+    private int eggs;
+
+
+    public Chicken(String name,int eggs){
+        this.name = name;
+        this.eggs = eggs;
+    }
+
+    //GETTERS SETTERS
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEggs() {
+        return eggs;
+    }
+
+    public void setEggs(int eggs) {
+        this.eggs = eggs;
+    }
+}
---
+++ src/test/java/ChickenTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class ChickenTest {
+
+    Chicken chick;
+
+    @BeforeEach
+    public void setUp() {
+        chick = new Chicken("Koko", 5);
+    }
+
+    //GETTER SETTER TEST SUITE
+    @Test
+    public void hasName(){
+        assertThat(chick.getName()).isEqualTo("Koko");
+    }
+
+        @Test
+    public void canSetName(){
+        chick.setName("Marta");
+        assertThat(chick.getName()).isEqualTo("Marta");
+    }
+
+        @Test
+    public void hasEggs(){
+        assertThat(chick.getEggs()).isEqualTo(5);
+    }
+
+        @Test
+    public void canSetEggs(){
+        chick.setEggs(10);
+        assertThat(chick.getEggs()).isEqualTo(10);
+    }
+
+
+}
